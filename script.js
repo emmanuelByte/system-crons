ef6d3d95a15c4b6c646593a2dc61e1ae8bb2e127
emmanuelbyte <emanuel4mechie@gmail.com>
2022-06-23 12:28:31 -0700

    plying

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const shell = require('shelljs')
-console.log("Running Script from Node")
+//console.log("Running Script from Node")
 
 var cron = require('node-cron');
 
-cron.schedule('*/5 * * * * *', () => {
-  console.log('running a task every 5 seconds');
+cron.schedule('* * * * * *', () => {
+  //console.log('running a task every 5 seconds');
  shell.exec('./githubcron.sh')
 })
