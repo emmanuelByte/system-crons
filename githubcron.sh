f08df645195131c4d9a280851356b284311d0b36
emanuelone <emanuel4mechie@gmail.com>
2022-06-23 18:25:42 +0100

    bustling

--- githubcron.sh
+++ githubcron.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 IP=$(curl -s -o /dev/null -w "%{http_code}"  https://github.com/)
 WORD=$(cat /usr/share/dict/words | sort -R | head -1)
-echo "$WORD"
+#echo "$WORD"
 #echo "$IP"
 
 if [ "$IP" == 200 ]
 then
    echo "start git operation" &&
-   echo -e 'd' >> update.txt &&
-   git add . && git commit -m "gh" && git push origin master && echo "push successfull 🤩" 
+   echo -e "$WORD" >> update.txt &&
+   git add . && git commit -m "$WORD" && git push origin master && echo "push successfull 🤩" 
 
 else
    echo "no internet service"
