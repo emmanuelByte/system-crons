fd6bf20d58c5ab76a587107755df3e6a5dc25e7f
emmanuelbyte <emanuel4mechie@gmail.com>
2022-06-23 12:23:20 -0700

    Jolson

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Running Script from Node")
 
 var cron = require('node-cron');
 
-cron.schedule('*/5 * * * *', () => {
-  console.log('running a task every 5 minute');
+cron.schedule('*/5 * * * * *', () => {
+  console.log('running a task every 5 seconds');
  shell.exec('./githubcron.sh')
 })
