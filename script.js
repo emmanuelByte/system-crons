8ccc48a35109581af5bd6efbc2ad3f185ea20a68
emanuelone <emanuel4mechie@gmail.com>
2022-06-23 19:16:00 +0100

    node_modules be gone!

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const shell = require('shelljs')
 console.log("Running Script from Node")
+
+var cron = require('node-cron');
+
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute');
  shell.exec('./githubcron.sh')
+})
