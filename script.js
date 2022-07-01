344c57d7367f382952cdcab992194629ae172a4d
Emmanuel Ifeanyi MECHIE <64092981+emmanuelByte@users.noreply.github.com>
2022-07-01 10:37:54 +0100

    change hour

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const shell = require('shelljs')
+const shell = require("shelljs");
 //console.log("Running Script from Node")
 
-var cron = require('node-cron');
-
-cron.schedule('0 */1 * * *', () => {
-  //console.log('running a task every 1 hour');
- shell.exec('./githubcron.sh')
-})
+function genCronString() {
+    const rand = Math.floor(Math.random() * (10 - 1)) + 1;
+    return `0 */${rand} * * *`;
+}
+(function cronJob() {
+    const cron = require("node-cron");
+    const cronExp = genCronString();
+    console.log(`Cron Expression: ${cronExp}`);
+    const day = new Date().getDay();
+    cron.schedule(cronExp, () => {
+        //console.log('running a task every 1 hour');
+        // shell.exec("./githubcron.sh");
+        const newDay = new Date().getDay();
+        if (day !== newDay) {
+            cron.stop();
+            cronJob();
+        }
+    });
+})();
